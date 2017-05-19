8ce0ef0f6a075f18af065af701df0889eaa2a543
André M. Ribeiro-dos-Santos <andremrsantos@gmail.com>
2017-05-19 09:23:44 -0300

    updated base-extension

--- emacs.d/elisp/base-extensions.el
+++ emacs.d/elisp/base-extensions.el
@@ -83,12 +83,12 @@
 
 ;; (use-package magit-popup)
 
-(use-package multiple-cursors
-  :bind
-  ("C-S-c C-S-c" . mc/edit-lines)
-  ("C->" . mc/mark-next-like-this)
-  ("C-<" . mc/mark-previous-like-this)
-  ("C-c C->" . mc/mark-all-like-this))
+;; (use-package multiple-cursors
+;;   :bind
+;;   ("C-S-c C-S-c" . mc/edit-lines)
+;;   ("C->" . mc/mark-next-like-this)
+;;   ("C-<" . mc/mark-previous-like-this)
+;;   ("C-c C->" . mc/mark-all-like-this))
 
 (use-package neotree
   :config
